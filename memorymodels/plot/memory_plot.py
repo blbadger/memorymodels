efa5e9d51510ea9c28eaf62ce1ef60f3ca3c28aa
Benjamin Badger <blbadger10@gmail.com>
2025-08-28 13:38:55 -0400

    minor updates

--- memorymodels/plot/memory_plot.py
+++ memorymodels/plot/memory_plot.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import json
 import matplotlib
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import FormatStrFormatter
 
 def plot_curves(input_paths, labels, plot_training=False):
     
@@ -34,44 +39,152 @@ def plot_curves(input_paths, labels, plot_training=False):
 
     plt.legend(fontsize='large')
     plt.tick_params(labelsize=16)
-    # plt.xscale('log')
+    plt.xscale('log')
     plt.xlabel('Thousand Steps', fontsize='x-large')
     plt.ylabel('Cross-Entropy Loss', fontsize='x-large')
     plt.show()
     plt.close()
     return
 
-paths = [
-    # "/Users/bbadger/Desktop/fineweb_c256x4_nomemorytrans.json",
-    # "/Users/bbadger/Desktop/fineweb_c256x4_memorytrans.json",
-    # "/Users/bbadger/Desktop/fineweb_c1024_transformer.json",
-    "/Users/bbadger/Desktop/fineweb_c256x4_nomemorymixer.json",
-    "/Users/bbadger/Desktop/fineweb_c256x4_memorymixer.json",
-    # "/Users/bbadger/Desktop/fineweb_c1024_mixer_rpad.json",
-    "/Users/bbadger/Desktop/fineweb_rmm.json"
+def plot_differential(input_paths, labels):
+    nomemory_loss = []
+    memory_loss = []
+    frozen_memory_loss = []
+    steps = []
+
+    for i, input_path in enumerate(input_paths):
+        for iteration in json.load(open(input_path))['log_history']:
+            step = iteration['step']
+
+            if 'eval_loss' in iteration and 'nomemory' in input_path:
+                loss = iteration['eval_loss']
+                nomemory_loss.append(loss)
+                steps.append(step/1000)
+            
+            if 'eval_loss' in iteration and 'frozen_mem' in input_path:
+                loss = iteration['eval_loss']
+                frozen_memory_loss.append(loss)
+            
+            if 'eval_loss' in iteration and '4_memory' in input_path:
+                loss = iteration['eval_loss']
+                memory_loss.append(loss)
+
+    min_len = min(len(nomemory_loss), len(memory_loss), len(frozen_memory_loss))
+    steps = steps[:min_len]
+    memory_loss_diff = [i - j for i, j in zip(nomemory_loss, memory_loss)]
+    frozen_loss_diff = [i - j for i, j in zip(nomemory_loss, frozen_memory_loss)]
+    relative_benefit = [i/j for i, j in zip(frozen_loss_diff, memory_loss_diff)]
     
-]
+    # plt.plot(steps, relative_benefit)
+    # plt.scatter(steps, relative_benefit)
+
+    # plt.legend(fontsize='large')
+    # plt.tick_params(labelsize=16)
+    # # plt.xscale('log')
+    # plt.xlabel('Thousand Steps', fontsize='x-large')
+    # plt.ylabel('Fraction of Loss Recovered', fontsize='x-large')
+    # plt.show()
+    # plt.close()
+
+    architectures = ['mixer' for i in range(12)]
+    print (len(relative_benefit), len(steps), len(architectures))
+    data = {
+        'Loss Difference': relative_benefit,
+        'Step': steps,
+        'Architecture': architectures
+    }
+    dataframe = pd.DataFrame(data)
+    print (dataframe)
+    sns.set_theme(style="darkgrid")
+    g = sns.scatterplot(data=dataframe, x='Step', y='Loss Difference', hue='Architecture')
+    g = sns.regplot(data=dataframe, x='Step', y='Loss Difference', scatter=False, logx=True)
+    # Add labels and title
+    # plt.legend(fontsize='x-large')
+    # plt.xscale('log')
+    g.set_yticklabels(g.get_yticks(), size = 16)
+    g.set_xticklabels(g.get_xticks(), size = 16)
+    g.get_yaxis().set_major_formatter(FormatStrFormatter('%.1f'))
+    g.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+
+
+    plt.xlabel('Step', fontsize='large')
+    plt.ylabel('Fraction of Memory Loss', fontsize='large')
+    # plt.ylim(0, 4)
+    plt.tight_layout()
+    plt.show()
+    return
+
+# paths = [
+#     "/Users/bbadger/Desktop/fineweb_c256x4_nomemorytrans.json",
+#     # "/Users/bbadger/Desktop/fineweb_c256x4_memorytrans.json",
+#     "/Users/bbadger/Desktop/fixed_position_mem_transformer.json",
+#     "/Users/bbadger/Desktop/fineweb_rmt_256x4.json",
+#     # "/Users/bbadger/Desktop/fineweb_c1024_transformer.json",
+#     # "/Users/bbadger/Desktop/fineweb_c256x4_nomemorymixer.json",
+#     # "/Users/bbadger/Desktop/fineweb_c256x4_memorymixer.json",
+#     # "/Users/bbadger/Desktop/fineweb_c1024_mixer_rpad.json",
+#     # "/Users/bbadger/Desktop/fineweb_rmm.json"
+    
+# ]
+
+# labels = [
+#     'No Memory Transformer',
+#     'Memory Transformer',
+#     "Recurrent Memory Transformer",
+#     # 'Full-context Transformer',
+#     # 'No Memory Mixer',
+#     # 'Memory Mixer',
+#     # 'Full-context Mixer',
+#     # 'Recurrent memory mixer'
+# ]
+
+paths = [
+    "/Users/bbadger/Desktop/fineweb_512x4_nomemory_mixer.json",
+    "/Users/bbadger/Desktop/fineweb_512x4_frozen_mem_mixer.json",
+    "/Users/bbadger/Desktop/fineweb_512x4_memory_mixer.json",
+    "/Users/bbadger/Desktop/fineweb_512x4_rmm.json",
+    # "/Users/bbadger/Desktop/fineweb_512x4_nonmemory_transformer.json",
+    # "/Users/bbadger/Desktop/fineweb_frozen_memory_transformer.json",
+    # "/Users/bbadger/Desktop/fineweb_512x4_frozen_transmem_transformer.json",
+    # "/Users/bbadger/Desktop/fineweb_512x4_memory_transformer.json",
+    # "/Users/bbadger/Desktop/fineweb_512x4_rmt.json",
+    ]
 
 labels = [
-    # 'No Memory Transformer',
-    # 'Memory Transformer',
-    # 'Full-context Transformer',
-    'No Memory Mixer',
-    'Memory Mixer',
-    # 'Full-context Mixer',
-    'Recurrent memory mixer'
+    "No Memory Mixer",
+    "Frozen Memory Mixer",
+    "Memory Mixer",
+    "Recurrent Memory Mixer",
+    # "No Memory Transformer",
+    # "Frozen Memory (mixer) Transformer",
+    # "Frozen Memory (transformer) Transformer", 
+    # "Memory Transformer",
+    # "Recurrent Memory Transformer"
 ]
 
-num_plots = 3
-colormap = plt.cm.Blues
+# paths = [
+#     "/Users/bbadger/Desktop/finemath_512x4_nomemory_mixer.json",
+#     "/Users/bbadger/Desktop/finemath_512x4_memory_mixer.json"
+# ]
+
+# labels = [
+#     "memory",
+#     "no memory"
+# ]
+
+num_plots = 4
+colormap = plt.cm.Reds
 colormap2 = plt.cm.Reds
 cmap_cycler = plt.cycler('color', colormap(np.linspace(0.2, 1, num_plots)))
-# plt.gca().set_prop_cycle(cmap_cycler)
-cmap2_cycler = plt.cycler('color', colormap2(np.linspace(0.2, 1, num_plots)))
-all_cycler = cmap_cycler.concat(cmap2_cycler)
+plt.gca().set_prop_cycle(cmap_cycler)
+# cmap2_cycler = plt.cycler('color', colormap2(np.linspace(0.2, 1, num_plots)))
+# all_cycler = cmap_cycler.concat(cmap2_cycler)
            
-plt.gca().set_prop_cycle(all_cycler)
+# plt.gca().set_prop_cycle(all_cycler)
 
-plot_curves(paths, labels)
+# plot_curves(paths, labels)
+plot_differential(paths, labels)
+
+
 
 

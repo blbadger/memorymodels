1c1320ac14180b9617dc1f0cb5ab452a8d82d563
Benjamin Badger <blbadger10@gmail.com>
2025-09-22 12:33:03 -0400

    add attributions

--- memorymodels/attributions_dataset.py
+++ memorymodels/attributions_dataset.py
@@ -14,10 +14,21 @@ attributions_dataset = concatenate_datasets(attributions_datasets)
 
 # flatten attribution dataset and rename col to match token dataset
 attributions_dataset.set_format('torch', columns=['ids', 'memory_attribution'])
+id_to_index = {}
+for i, batch in enumerate(attributions_dataset):
+    for id in batch['ids']:
+        id_to_index[id] = i
 attributions_dataset.rename_column('ids', 'id')
+
+def add_attribution(example):
+    id = example['id']
+    batch_index = id_to_index[id]
+    attribution = attributions_dataset[batch_index][id]
+    example['attribution'] = attribution
+    return example
 
 # load token dataset, join on id, and save
 token_dataset = load_from_disk(f"{data_root}/fineweb-edu-tokenized-train-c1024-lpad-8k")
-print (token_dataset[0], attributions_dataset[0])
-all_dataset = token_dataset.join(attributions_dataset, on='id', how='outer')
-all_dataset.save_to_disk(f"{data_root}/fineweb-edu-tokenized-train-c1024-lpad-attr-8k")
+token_dataset.map(add_attribution)
+print(token_dataset[0])
+token_dataset.save_to_disk(f"{data_root}/fineweb-edu-tokenized-train-c1024-lpad-attr-8k")

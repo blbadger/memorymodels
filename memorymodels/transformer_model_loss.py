f479a44662ad97e38d3c82c1aea094a5b4b1c86b
Benjamin Badger <blbadger10@gmail.com>
2025-09-29 17:30:33 -0400

    loss dataset gen

--- memorymodels/transformer_model_loss.py
+++ memorymodels/transformer_model_loss.py
@@ -27,89 +27,145 @@ from dotenv import load_dotenv
 
 warnings.filterwarnings(action='ignore')
 
-load_dotenv()
-checkpoint_root = os.getenv('CHECKPOINT_ROOT')
-data_root = os.getenv('DATA_ROOT')
 
-device = 'cuda' if torch.cuda.is_available else 'cpu'
-decoder_dim = 512
-context_length = 1024
-n_layers = 16
-n_heads = 4
-
-vocab_size = 8000
-llama_config_kwargs = {
-	'hidden_size': decoder_dim,
-	'intermediate_size': 4*decoder_dim,
-	'num_hidden_layers': n_layers,
-	'num_attention_heads': n_heads,
-	'vocab_size': vocab_size
-}
-
-def unreduced_loss(model_output, input_tokens, *args, **kwargs):
+def unreduced_loss(model_output, input_tokens, reduction=False, *args, **kwargs):
     target = input_tokens[:, 1:]
     model_output = rearrange(model_output.logits, 'b t e -> b e t')[:, :, :-1]
     loss = loss_fn(model_output, target) 
-    non_pad_tokens = torch.sum(torch.where(input_tokens==-100, 0, 1))
-    loss = torch.sum(loss)/non_pad_tokens
+    if reduction:
+        non_pad_tokens = torch.sum(torch.where(input_tokens==-100, 0, 1))
+        loss = torch.sum(loss)/non_pad_tokens
     return loss
 
-# Initializing a LLaMA model
-configuration = LlamaConfig(**llama_config_kwargs)
+def test_losses():
+    test_path = f"{data_root}/fineweb-edu-tokenized-test-c1024-lpad-8k"
+    take_n = 32
+    test_dataset = load_from_disk(test_path, keep_in_memory=None).take(take_n)
+    print_attributions = {}
 
-# Initializing a model from the llama-7b style configuration
-model = LlamaForCausalLM(configuration).float().to('cuda')
-safetensors.torch.load_model(model, f"{checkpoint_root}/fineweb_transformer_512_n16_c1024_b64_extended/checkpoint-500000/model.safetensors")
-model = model.eval()
+    print_losses = {}
+    batch = test_dataset[:take_n]
+    input_ids = torch.stack([torch.tensor(l) for l in batch['input_ids']], dim=0).to('cuda')
+    attention_mask = torch.stack([torch.tensor(l) for l in batch['attention_mask']], dim=0).to('cuda')
+    output = model(input_ids, attention_mask=attention_mask, labels=input_ids)
+    reshaped_output = rearrange(output.logits, 'b t e -> b e t')
 
-tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
-tokenizer.pad_token = tokenizer.eos_token
-n_vocab = len(tokenizer)
-test_path = f"{data_root}/fineweb-edu-tokenized-test-c1024-lpad-8k"
-take_n = 32
-test_dataset = load_from_disk(test_path, keep_in_memory=None).take(take_n)
-print_attributions = {}
+    loss_fn = nn.CrossEntropyLoss(reduction='none')
+    losses = loss_fn(reshaped_output[:, :, :-1], input_ids[:, 1:])
 
-print_losses = {}
-batch = test_dataset[:take_n]
-input_ids = torch.stack([torch.tensor(l) for l in batch['input_ids']], dim=0).to('cuda')
-attention_mask = torch.stack([torch.tensor(l) for l in batch['attention_mask']], dim=0).to('cuda')
-output = model(input_ids, attention_mask=attention_mask, labels=input_ids)
-reshaped_output = rearrange(output.logits, 'b t e -> b e t')
+    for i in range(take_n):
+        if test_dataset[i]['input_ids'][0] != 1:
+            print_losses[test_dataset[i]['id']] = losses[i].tolist()
 
-loss_fn = nn.CrossEntropyLoss(reduction='none')
-losses = loss_fn(reshaped_output[:, :, :-1], input_ids[:, 1:])
+    d = {'losses': print_losses}
+    with open('/home/badger/clm_losses.json', 'w') as f:
+        json.dump(d, f)
+    	
+    training_arguments = transformers.TrainingArguments(
+            num_train_epochs=3,
+            per_device_train_batch_size=64,
+            per_device_eval_batch_size=64,
+            warmup_steps=500,
+            eval_steps=4000,
+            save_steps=8000,
+            learning_rate=2e-4, 
+            fp16=True, 
+            eval_strategy='steps',
+            output_dir='/home/badger',
+            optim='adamw_torch',
+            overwrite_output_dir=False,
+            max_steps=200000
+    )
 
-for i in range(take_n):
-    if test_dataset[i]['input_ids'][0] != 1:
-        print_losses[test_dataset[i]['id']] = losses[i].tolist()
+    trainer = transformers.Trainer(
+            model=model,
+            train_dataset=test_dataset,
+            eval_dataset=test_dataset,
+            args=training_arguments,
+            data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+    	   compute_loss_func=unreduced_loss
+    	)
+    print (trainer.evaluate())
+    return
 
-d = {'losses': print_losses}
-with open('/home/badger/clm_losses.json', 'w') as f:
-    json.dump(d, f)
-	
-training_arguments = transformers.TrainingArguments(
-        num_train_epochs=3,
-        per_device_train_batch_size=64,
-        per_device_eval_batch_size=64,
-        warmup_steps=500,
-        eval_steps=4000,
-        save_steps=8000,
-        learning_rate=2e-4, 
-        fp16=True, 
-        eval_strategy='steps',
-        output_dir='/home/badger',
-        optim='adamw_torch',
-        overwrite_output_dir=False,
-        max_steps=200000
-)
 
-trainer = transformers.Trainer(
-        model=model,
-        train_dataset=test_dataset,
-        eval_dataset=test_dataset,
-        args=training_arguments,
-        data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
-	compute_loss_func=unreduced_loss
-	)
-print (trainer.evaluate())
+if __name__ == '__main__':
+
+    load_dotenv()
+    checkpoint_root = os.getenv('CHECKPOINT_ROOT')
+    data_root = os.getenv('DATA_ROOT')
+
+    device = 'cuda' if torch.cuda.is_available else 'cpu'
+    decoder_dim = 512
+    context_length = 1024
+    n_layers = 16
+    n_heads = 4
+
+    vocab_size = 8000
+    llama_config_kwargs = {
+        'hidden_size': decoder_dim,
+        'intermediate_size': 4*decoder_dim,
+        'num_hidden_layers': n_layers,
+        'num_attention_heads': n_heads,
+        'vocab_size': vocab_size
+    }
+
+    # Initializing a LLaMA model
+    configuration = LlamaConfig(**llama_config_kwargs)
+
+    # Initializing a model from the llama-7b style configuration
+    model = LlamaForCausalLM(configuration).float()
+    safetensors.torch.load_model(model, f'{checkpoint_root}/fineweb_memtrans_256c4_d512_n16_c1024_b16x4_extended/checkpoint-500000/model.safetensors', strict=True) # no decoder_input_embeds param in original model
+    model.eval()
+
+    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+    tokenizer.pad_token = tokenizer.eos_token
+    n_vocab = len(tokenizer)
+
+    use_ddp = True
+    if not use_ddp: 
+        device_id = 0   
+        model = model.to(device)
+    else:
+        gpu_count = torch.cuda.device_count()
+        dist.init_process_group("nccl")
+        rank = dist.get_rank()
+        device_id = rank % torch.cuda.device_count()
+        model = DDP(model.to(device_id), device_ids=[device_id])
+
+    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+    tokenizer.pad_token = tokenizer.eos_token
+    n_vocab = len(tokenizer)
+
+    train_path = f"{data_root}/fineweb-edu-tokenized-train-c1024-lpad-8k"
+    test_path = f"{data_root}/fineweb-edu-tokenized-test-c1024-lpad-8k"
+
+    # if you have a new dataset, map before loading from disk
+    datasets.config.IN_MEMORY_MAX_SIZE = 10e9
+    train_dataset = load_from_disk(train_path, keep_in_memory=None)
+    test_dataset = load_from_disk(train_path, keep_in_memory=None)
+
+    n_gpus = torch.cuda.device_count()
+    dataset_length = len(test_dataset)
+    device_chunk_size = int(dataset_length / n_gpus)
+    start, end = device_id * device_chunk_size, (device_id+1) * device_chunk_size
+    test_dataset = test_dataset.skip(start).take(end - start)
+    mlflow.end_run()
+    batch_size = 32
+    attributions = []
+    ids = []
+    if len(test_dataset) % batch_size == 0:
+        batches = len(test_dataset) // batch_size
+    else:
+        batches = len(test_dataset) // (batch_size) + 1
+
+    masks = []  
+    for sample_index in tqdm(range(batches)):
+        batch = test_dataset[sample_index*batch_size:sample_index*batch_size + batch_size]
+        mask = torch.tensor(batch['attention_mask'])
+        attributions.append(get_token_loss(model, batch) * mask)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    attributions_dict = {'memory_attribution': attributions, 'ids': ids}
+    attributions_dataset = Dataset.from_dict(attributions_dict)
+    attributions_dataset.save_to_disk(f"{data_root}/fineweb-edu-tokenized-train-loss-lpad-8k_{rank}")

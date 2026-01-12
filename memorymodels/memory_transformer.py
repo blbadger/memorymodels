3c16e67a702d17ebc6d4ce3a6a325674a154c242
Benjamin Badger <blbadger10@gmail.com>
2026-01-12 10:21:40 -0500

    combined memtrans

--- memorymodels/memory_transformer.py
+++ memorymodels/memory_transformer.py
@@ -224,6 +224,152 @@ class VariableMemoryTransformer(nn.Module):
 		all_outputs = torch.cat(all_outputs, dim=2) # concat in token dim
 		return mean_loss, all_outputs
 
+
+
+class ObjectiveMemoryTransformer(nn.Module):
+
+	def __init__(self, n_vocab, encoder_dim, dim, depth, length, objective='combined', compression=1, n_heads=4, n_chunks=4, fixed_memory=True, frozen_encoder=None, no_memory=False, decoder=None, blank_copy=False):
+		super().__init__()
+
+		self.no_memory = no_memory
+		self.decoder_dim = dim
+		if not self.no_memory:
+			if frozen_encoder:
+				for name, param in frozen_encoder.named_parameters():
+					param.requires_grad = False
+				self.encoder = frozen_encoder
+			else:
+				llama_config_kwargs = {
+					'hidden_size': encoder_dim,
+					'intermediate_size': 4*encoder_dim,
+					'num_hidden_layers': depth,
+					'num_attention_heads': n_heads,
+					'vocab_size': n_vocab
+				}
+				encoder_configuration = LlamaConfig(**llama_config_kwargs)
+				self.encoder = LlamaModel(encoder_configuration)
+		else:
+			self.encoder = None
+
+		self.wte = nn.Embedding(n_vocab, encoder_dim) #NB: originally dim, does not matter for frozen encoder
+		self.decoder_proj = None
+
+		if decoder:
+			if isinstance(decoder, PeftModelForCausalLM):
+				self.decoder = decoder.base_model.model.model
+				self.decoder_wte = decoder.base_model.model.model.embed_tokens
+				self.lm_head = decoder.base_model.model.lm_head
+			else:
+				self.decoder = decoder.model
+				self.decoder_wte = decoder.model.embed_tokens
+				self.lm_head = decoder.lm_head
+		else:
+			llama_config_kwargs = {
+				'hidden_size': dim,
+				'intermediate_size': 4*dim,
+				'num_hidden_layers': depth,
+				'num_attention_heads': n_heads,
+				'vocab_size': n_vocab
+			}
+			decoder_configuration = LlamaConfig(**llama_config_kwargs)
+			self.decoder = LlamaModel(decoder_configuration)
+			self.decoder_wte = nn.Embedding(n_vocab, dim)
+			self.lm_head = nn.Linear(dim, n_vocab, bias=False)
+
+		if encoder_dim != dim:
+			self.decoder_proj = nn.Linear(encoder_dim, dim) # project if necessary
+
+		self.cel = nn.CrossEntropyLoss()
+		self.tokenized_length = length
+		self.compression = compression > 1
+		self.chunks = n_chunks
+		self.fixed_memory = fixed_memory
+		if self.compression:
+			self.down = nn.Linear(encoder_dim, encoder_dim//compression)
+			self.up = nn.Linear(encoder_dim//compression, encoder_dim)
+		self.blank_copy = blank_copy
+		assert objective in ['copy', 'combined', 'clm'], 'Unrecognized objective function'
+		self.objective = objective
+		
+
+	def forward(self, input_ids, labels=None, attention_mask=None, **kwargs):
+		input_ids = input_ids.to(device)
+		all_inputs = []
+		if self.objective != 'clm':
+			copy_input_ids = copy_dataset(input_ids, blank_copy=self.blank_copy)
+			if labels is not None:
+				copy_labels = copy_labels(labels) # masks first half
+			if self.objective == 'combined':
+				all_inputs = [[input_ids, labels]]
+			all_inputs.append([copy_input_ids, copy_labels])
+
+		total_loss = 0
+		for input_ids, labels in all_inputs:
+			# generate encoder embeddings
+			embedding_array = []
+			i = 0
+			if self.no_memory:
+				i = 1e9
+				embedding_array = [torch.ones((input_ids.shape[0], 1, self.decoder_dim)).to(device) for _ in range(self.chunks)]
+
+			while input_ids.shape[1] - self.tokenized_length > i:
+				input_chunk, attention_chunk = self.wte(input_ids[:, i: i+self.tokenized_length]), attention_mask[:, i: i+self.tokenized_length]
+				
+				x = self.encoder(inputs_embeds=input_chunk, attention_mask=attention_chunk)
+				if not torch.is_tensor(x):
+					x = x.last_hidden_state
+				
+				encoder_embedding = x[:, -1, :].unsqueeze(1) # dim=[batch, token, hidden]
+				if self.compression:
+					encoder_embedding = self.down(encoder_embedding)
+					encoder_embedding = self.up(encoder_embedding)
+				if self.decoder_proj:
+					encoder_embedding = self.decoder_proj(encoder_embedding)
+				embedding_array.append(encoder_embedding)
+				i += self.tokenized_length
+
+			# embedding_array now stores length // n_ctx - 1 embeddings
+			input_embeddings = self.decoder_wte(input_ids)
+			all_outputs = []
+			for c in range(self.chunks): # self.chunks
+				decoder_embeds = input_embeddings[:, (c*self.tokenized_length):(c+1)*self.tokenized_length]
+				if self.fixed_memory:
+					pad = torch.ones((input_ids.shape[0], self.chunks-c, input_embeddings.shape[2])).to(device)
+					x = torch.cat((embedding_array[:c] + [pad] + [decoder_embeds]), dim=1) # concatenation on token dim
+				else:
+					x = torch.cat((embedding_array[:c] + [decoder_embeds]), dim=1) # concatenation on token dim
+				if attention_mask is not None:
+					attention_mask = torch.cat((torch.ones(input_ids.shape[0], c).to(device), attention_mask), dim=1)
+				
+				# feed pre-concatenated input embeddings to the transformer decoder
+				x = self.decoder(inputs_embeds=x, attention_mask=attention_mask)
+				output = self.lm_head(x.last_hidden_state)
+				if labels.dim() > 2:
+					labels = rearrange(labels, 'b p t -> b (p t)')
+				output = rearrange(output, 'b t e -> b e t')
+				
+				if self.fixed_memory:
+					all_outputs.append(output[..., self.chunks:self.chunks+self.tokenized_length]) # assemble all outputs
+					shift_logits = output[..., self.chunks:self.chunks+self.tokenized_length-1].contiguous()
+				else:
+					all_outputs.append(output[..., c:]) # assemble all outputs
+					shift_logits = output[..., c:-1].contiguous() # first c 'tokens' are encoding
+				shift_labels = labels[..., (c*self.tokenized_length)+1:(c+1)*(self.tokenized_length)].contiguous()
+				# only take loss of non-fully-masked blocks
+				if torch.all(shift_labels == -100):
+		 			continue
+				loss = self.cel(shift_logits, shift_labels)
+				if not torch.isnan(loss):
+					total_loss += loss
+			if total_loss == 0:
+				total_loss = loss # if no chunks are valid
+			total_loss += loss
+
+		mean_loss = total_loss / self.chunks
+		all_outputs = torch.cat(all_outputs, dim=2) # concat in token dim
+		return mean_loss, all_outputs
+
+
 class ParVarMemTransformer(nn.Module):
 	"""
 	Parallelized memory transformer
---
+++ memorymodels/memory_transformer_combined.py
@@ -0,0 +1,205 @@
+import os
+import torch
+import torch.nn as nn
+from einops import rearrange
+import transformers
+from transformers import AutoTokenizer
+import mlflow
+
+from datasets import load_dataset, load_from_disk
+import transformers
+from transformers import LlamaConfig, LlamaForCausalLM, LlamaModel
+from prettytable import PrettyTable
+from safetensors.torch import save_file
+from safetensors import safe_open
+import safetensors
+from safetensors.torch import load_model
+import datasets
+import warnings
+import shutil
+from dotenv import load_dotenv
+from pathlib import Path
+
+from mixer_autoencoder import AutoencodingMixer, TruncatedModel
+from transformer_autoencoder import AbbreviatedModel, AutoencodingTransformer, AutoencodingTransformerMod, UnrolledAutoencodingTransformer
+from memory_transformer import VariableMemoryTransformer, MemoryTransformer, RecurrentMemoryTransformer, ProjMemoryTransformer
+
+warnings.filterwarnings(action='ignore')
+
+load_dotenv()
+checkpoint_root = os.getenv('CHECKPOINT_ROOT')
+data_root = os.getenv('DATA_ROOT')
+
+device = 'cuda' if torch.cuda.is_available else 'cpu'
+
+def load_encoder():
+	encoder_dim = 512
+	decoder_dim = 512
+	context_length = 512
+	compression = 1
+	n_layers = 8
+	n_heads = 8
+
+	vocab_size = 8000
+
+	llama_config_kwargs = {
+		 'hidden_size':decoder_dim,
+		 'intermediate_size': 4*decoder_dim,
+		 'num_hidden_layers': n_layers,
+		 'num_attention_heads': n_heads,
+		 'vocab_size': vocab_size
+	 }
+
+	# Initializing a LLaMA model
+	configuration = LlamaConfig(**llama_config_kwargs)
+	encoder_model = AbbreviatedModel(LlamaForCausalLM(configuration), tokenized_length=context_length)
+	pretrained_autoencoder = UnrolledAutoencodingTransformer(vocab_size, decoder_dim, encoder_model, decoder_model, tokenized_length=context_length, compression=compression, freeze_encoder=False)
+	load_path = Path(f"{checkpoint_root}/fineweb_autotrans_unroll_1024c1_d1024_n8_c512_b32x2/checkpoint-200000/model.safetensors")
+
+	# load encoder
+	safetensors.torch.load_model(pretrained_autoencoder, load_path)
+	#encoder = TruncatedModel(pretrained_autoencoder)
+	encoder = pretrained_autoencoder.encoder
+	return encoder
+
+@torch.no_grad()
+def hamming(model_output, labels):
+	total_metric = 0
+	# assign and shift outputs and labels
+	labels= torch.tensor(labels)[..., 1:]
+	model_output = torch.tensor(model_output[0])[..., :-1]
+	nonpad_tokens = torch.where(labels != -100, 1, 0)
+	equal_tokens = torch.where(model_output == labels, 1, 0) & nonpad_tokens
+	average_metric = torch.sum(equal_tokens) / torch.sum(nonpad_tokens)
+	return torch.tensor([average_metric])
+
+def compute_hamming_metric(eval_preds):
+	logits, labels = eval_preds
+	hamming_metric = hamming(logits, labels)
+	return {'Hamming Distance': hamming_metric}
+
+def preprocess_logits_for_metrics(logits, labels):
+    """
+    Original Trainer has a memory leak: a workaround to avoid saving all tensors
+    """
+    pred_ids = torch.argmax(logits, dim=-2)
+    return pred_ids, labels
+
+
+tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+tokenizer.pad_token = tokenizer.eos_token
+n_vocab = len(tokenizer)
+
+encoder_dim = 512 
+decoder_dim = 512 
+context_length = 256 
+compression = 1 
+n_layers = 16
+n_heads = 4 
+
+vocab_size = 8000
+llama_config_kwargs = { 
+    'hidden_size':decoder_dim,
+    'intermediate_size': 4*decoder_dim,
+    'num_hidden_layers': n_layers,
+    'num_attention_heads': n_heads,
+    'vocab_size': vocab_size
+}
+
+# Initializing a LLaMA model
+#configuration = LlamaConfig(**llama_config_kwargs)
+#encoder_model = AbbreviatedModel(LlamaForCausalLM(configuration), tokenized_length=context_length)
+#decoder_model = AbbreviatedModel(LlamaForCausalLM(configuration), tokenized_length=context_length)
+
+
+# load a pretrained autoencoder
+#model = UnrolledAutoencodingTransformer(vocab_size, decoder_dim, encoder_model, decoder_model, tokenized_length=context_length, compression=compression, freeze_encoder=False)
+#model = LlamaForCausalLM(configuration)
+#load_model(model, '/home/bbadger/Desktop/fineweb_autoencoding_transformer_512c1_d512_n8_c256_b32x4/checkpoint-200000/model.safetensors')
+#encoder = model.encoder.model
+
+#load_model(model, '/home/bbadger/Desktop/fineweb_training/fineweb_llama_n16_h4_b32/checkpoint-200000/model.safetensors')
+#encoder = model.model
+
+encoder_dim = 512
+decoder_dim = 512
+context_length = 256
+compression = 1 
+n_layers = 16 
+n_heads = 4
+model = ObjectiveMemoryTransformer(n_vocab, encoder_dim, decoder_dim, n_layers, context_length, objective='combined', n_heads=n_heads, n_chunks=4, fixed_memory=True, frozen_encoder=None, no_memory=False, blank_copy=False)
+
+# load the pretrained memory model
+#load_model(model, '/home/bbadger/Desktop/fineweb_copy_memtrans_frozenenc_nodecoder_c256x4_512c1_d512_n16_c256_b8x4/checkpoint-100000/model.safetensors')
+
+print (model)
+
+train_path = f"{data_root}/fineweb-edu-tokenized-train-c1024"
+test_path = f"{data_root}/fineweb-edu-tokenized-test-c1024"
+
+# load datasets and duplicate entries
+datasets.config.IN_MEMORY_MAX_SIZE = 3e9
+train_dataset = load_from_disk(train_path)
+test_dataset = load_from_disk(test_path).take(10000).filter(lambda x: x['input_ids'][-1] != 1, num_proc=16)
+
+total_batch_size = 64
+n_devices = 4
+# get number of devices (assumes that all visible devices are used for training)
+if torch.cuda.is_available():
+	n_devices = torch.cuda.device_count()
+batch_per_device = 8
+gradient_accumulation_steps = total_batch_size // (n_devices * batch_per_device)
+# descriptive name for output
+output_dir = f'{checkpoint_root}/fineweb_combined_memtrans_c256x4\
+_{encoder_dim}\
+c{compression}\
+_d{decoder_dim}\
+_n{n_layers}\
+_c{context_length}_b{batch_per_device}x{n_devices}x{gradient_accumulation_steps}'
+
+mlflow.end_run()
+training_arguments = transformers.TrainingArguments(
+	num_train_epochs=3,
+	per_device_train_batch_size=batch_per_device,
+	per_device_eval_batch_size=batch_per_device,
+	gradient_accumulation_steps=gradient_accumulation_steps,
+	warmup_steps=500,
+	eval_steps=500,
+	save_steps=10000,
+	learning_rate=2e-4, 
+	fp16=True,
+	eval_strategy='steps',
+	output_dir=output_dir,
+	optim='adamw_torch',
+	overwrite_output_dir=True,
+	max_steps=100000,
+    torch_compile=True
+)
+
+trainer = transformers.Trainer(
+	model=model,
+	train_dataset=train_dataset,
+	eval_dataset=test_dataset,
+	args=training_arguments,
+	data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+	compute_metrics = compute_hamming_metric,
+	preprocess_logits_for_metrics=preprocess_logits_for_metrics
+)
+
+# save driver code snapshot in checkpoint dir
+code_path = os.path.abspath(__file__)
+if not os.path.isdir(output_dir):
+	os.mkdir(output_dir)
+shutil.copy(code_path, output_dir)
+
+print (f"training begun: saving results in {output_dir}")
+model.train()
+trainer.train()
+
+model.objective = 'copy'
+print (f'Evaluating Copy Accuracy:')
+trainer.evaluate()
+print ('='*90)
+print (f'Evaluating CLM Accuracy:')
+model.objective = 'clm'
+trainer.evaluate()

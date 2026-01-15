d9903aef9aca3b7dce81773cd055ab170f099a61
Benjamin Badger <blbadger10@gmail.com>
2026-01-15 12:16:32 -0500

    autoencoder retr

--- memorymodels/retrieval/infonce_ngram_retrieval.py
+++ memorymodels/retrieval/infonce_ngram_retrieval.py
@@ -8,12 +8,13 @@ from transformers import AutoTokenizer
 from safetensors.torch import load_model, safe_open
 import random
 import threading
-from transformer_autoencoder import UnrolledAutoencodingTransformer
 import os
 from dotenv import load_dotenv
 import warnings
 import datasets
 from datasets import load_from_disk
+
+from transformer_autoencoder import UnrolledAutoencodingTransformer
 
 class RetrievalTransformer(nn.Module):
 
@@ -51,43 +52,50 @@ class RetrievalTransformer(nn.Module):
 		loss = infoNCEloss(x, matching_index=matching_index, embedding_index=embedding_indices)
 		return loss, x
 
-
 class RetrievalAutoencoderTransformer(nn.Module):
 
 	def __init__(self, model, pad_token_id=1, padding_side='right', ngram_size=7):
 		super().__init__()
-		self.model = model.encoder.model
-		self.wte = model.wte
+		if isinstance(self.encoder, LlamaModel):
+			self.wte = model.wte
+		else:
+			self.wte = None
+
+		self.model = model.encoder # assumes that the encoder is a LlamaModel with trained wte
 		self.ngram_size = ngram_size
 		self.pad_token_id = pad_token_id
+		self.padding_side = padding_side
 
 	def select_ngram(self, input_ids):
 		# expects inputs to be of shape [b, t]
-		sample_index = random.randint(1, input_ids.shape[0]) # the first sample will be replaced with the input
-		ngram_index = random.randint(0, input_ids.shape[1] - ngram_size)
-		sample_ngram = input_ids[sample_index, ngram_index: ngram_index + ngram_size]
+		sample_index = random.randint(1, input_ids.shape[0]-1) # the first sample will be replaced with the input
+		ngram_index = random.randint(0, input_ids.shape[1] - self.ngram_size)
+		sample_ngram = input_ids[sample_index, ngram_index: ngram_index + self.ngram_size]
 		return sample_index, sample_ngram
 
 	def forward(self, input_ids, attention_mask, labels=None, **kwargs):
-		if self.prebatched:
+		if input_ids.dim() > 2:
 			input_ids = input_ids.squeeze(0) # p b t -> b t
 		matching_index, sample_ngram = self.select_ngram(input_ids)
-		pad_ngram = (torch.ones(input_ids.shape[1] - ngram_size) * self.pad_token_id).to(torch.long)
+		pad_ngram = (torch.ones(input_ids.shape[1] - self.ngram_size) * self.pad_token_id).to(torch.long).to(input_ids.device)
 		pad_attention = torch.zeros
-		if padding_side == 'right':
+		if self.padding_side == 'right':
 			sample_ngram = torch.cat((sample_ngram, pad_ngram), dim=0)
-			attention_ngram = torch.cat((torch.ones(ngram_size, dtype=torch.long), torch.zeros(input_ids.shape[1] - ngram_size, dtype=torch.long)), dim=0)
-		elif padding_side == 'left':
+			attention_ngram = torch.cat((torch.ones(self.ngram_size, dtype=torch.long), torch.zeros(input_ids.shape[1] - self.ngram_size, dtype=torch.long)), dim=0).to(input_ids.device)
+		elif self.padding_side == 'left':
 			sample_ngram = torch.cat((pad_ngram, sample_ngram), dim=0)
-			attention_ngram = torch.cat((torch.zeros(input_ids.shape[1] - ngram_size, dtype=torch.long), torch.ones(ngram_size, dtype=torch.long)), dim=0)
+			attention_ngram = torch.cat((torch.zeros(input_ids.shape[1] - self.ngram_size, dtype=torch.long), torch.ones(self.ngram_size, dtype=torch.long)), dim=0).to(input_ids.device)
 
 		input_ids[0] = sample_ngram # zeroth batch element is the query phrase
 		attention_mask[0] = attention_ngram
-		x = self.wte(input_ids)
-		x = self.model(inputs_embeds=x, attention_mask=attention_mask).last_hidden_state
+		if self.wte:
+			x = self.wte(input_ids)
+		else:
+			x = input_ids
+		x = self.model(x) # no attention mask for autoencoder training
 		embedding_indices = -1
-		loss = infoNCEloss(model_output, matching_index=matching_index, embedding_index=embedding_indices)
-		return loss, model_output
+		loss = infoNCEloss(x, matching_index=matching_index, embedding_index=embedding_indices)
+		return loss, x
 
 
 def infoNCEloss(output, matching_index=None, embedding_index=-2, temp=0.02):
@@ -141,7 +149,7 @@ if __name__ == '__main__':
 	tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
 	tokenizer.pad_token = tokenizer.eos_token
 
-	tokenized_length = 512
+	tokenized_length = 256
 	dim = 512
 	n_layers = 16
 	n_heads = 4
@@ -159,18 +167,35 @@ if __name__ == '__main__':
 	print (llama_config_kwargs)
 	# Initializing a LLaMA model
 	configuration = LlamaConfig(**llama_config_kwargs)
-	model = LlamaForCausalLM(configuration)
+
 	# loads a pretrained (on FineWeb) CLM model
-	load_model(model, f"{checkpoint_root}/fineweb_training/fineweb_llama_n16_h4_b32/checkpoint-200000/model.safetensors")
-	model = RetrievalTransformer(model, ngram_size=ngram, padding_side='right', pad_token_id=tokenizer.pad_token_id)
+	# model = LlamaForCausalLM(configuration)
+	# load_model(model, f"{checkpoint_root}/fineweb_training/fineweb_llama_n16_h4_b32/checkpoint-200000/model.safetensors")
+	# model = RetrievalTransformer(model, ngram_size=ngram, padding_side='right', pad_token_id=tokenizer.pad_token_id)
+
+	# train_path = f"{data_root}/fineweb-edu-tokenized-train-c512"
+	# test_path = f"{data_root}/fineweb-edu-tokenized-test-c512"
+	# datasets.config.IN_MEMORY_MAX_SIZE = 1e9
+	# train_dataset = load_from_disk(train_path)
+	# test_dataset = load_from_disk(test_path)
+
+	encoder_model = AbbreviatedModel(LlamaForCausalLM(configuration), tokenized_length=context_length)
+	decoder_model = AbbreviatedModel(LlamaForCausalLM(configuration), tokenized_length=context_length)
+	model = UnrolledAutoencodingTransformer(vocab_size, decoder_dim, encoder_model, decoder_model, tokenized_length=context_length, compression=compression, freeze_encoder=False)
+	load_model(autoencoder, '/home/bbadger/Desktop/fineweb_autoencoding_transformer_512c1_d512_n8_c256_b32x4/checkpoint-200000')
+	model = RetreivalAutoencoderTransformer(model, ngram_size=ngram, padding_side='right', pad_token_id=tokenizer.pad_token_id)
+
+	def half_data(example):
+	    example['input_ids'] = example['input_ids'][256:]
+	    if 'attention_mask' in example:
+	        example['attention_mask'] = example['attention_mask'][256:]
+	    return example
+
+	datasets.config.IN_MEMORY_MAX_SIZE = 1e9 
+	train_dataset = load_from_disk(train_path).map(half_data, batched=False, num_proc=16)
+	test_dataset = load_from_disk(test_path).map(half_data, batched=False, num_proc=16)
+
 	tokenizer.pad_token = tokenizer.eos_token
-
-	train_path = f"{data_root}/fineweb-edu-tokenized-train-c512"
-	test_path = f"{data_root}/fineweb-edu-tokenized-test-c512"
-	datasets.config.IN_MEMORY_MAX_SIZE = 1e9
-	train_dataset = load_from_disk(train_path)
-	test_dataset = load_from_disk(test_path)
-
 	pad_token = int(tokenizer.encode(tokenizer.pad_token)[-1])
 
 	batch_size = 32
